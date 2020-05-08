3b89c6b64a2c87e255a7466aed19e3953ba06511
Ravindranath Akila <ravindranathakila@gmail.com>
2020-05-08 20:00:33 +0800

    Implementation done, with test cases

--- src/main/java/identifiedcacheditem/identifiedcacheditem/ToIsIdentifiedCachedItemImplBaseImpl.java
+++ src/main/java/identifiedcacheditem/identifiedcacheditem/ToIsIdentifiedCachedItemImplBaseImpl.java
@@ -1,23 +1,78 @@
 package identifiedcacheditem.identifiedcacheditem;
 
+import com.google.protobuf.ByteString;
+import com.hazelcast.core.Hazelcast;
+import com.hazelcast.core.HazelcastInstance;
+import com.hazelcast.core.ILock;
+import id.id.IsId;
+import identifiedcacheditem.input.IsInput;
+import identifiedcacheditem.output.IsOutput;
+import io.grpc.Status;
+import io.grpc.StatusRuntimeException;
 import io.grpc.stub.StreamObserver;
 
+import java.util.Map;
 import java.util.logging.Logger;
 
 public class ToIsIdentifiedCachedItemImplBaseImpl extends ToIsIdentifiedCachedItemGrpc.ToIsIdentifiedCachedItemImplBase {
 
-    private final Logger log;
+    private final Logger logger;
+
+    private final HazelcastInstance hzInstance;
+    private final Map<String, ByteString> general;
+    private final ILock lock;
+
 
     public ToIsIdentifiedCachedItemImplBaseImpl() {
-        log = Logger.getLogger(getClass().getName());
-        log.info("starting");
+        logger = Logger.getLogger(getClass().getName());
+        logger.info("starting");
 
-        log.info("started");
+        logger.info("starting hazelcast");
+        hzInstance = Hazelcast.newHazelcastInstance();
+        logger.info("started hazelcast");
+
+        logger.info("starting general map: hazelcast");
+        general = hzInstance.getMap("general");
+        logger.info("started general map: hazelcast");
+
+        logger.info("starting general lock: hazelcast");
+        lock = hzInstance.getLock("general");
+        logger.info("started general lock: hazelcast");
+
+        logger.info("started");
     }
 
     @Override
     public void produce(final NotIdentifiedCachedItem request, final StreamObserver<IsIdentifiedCachedItem> responseObserver) {
+        final IsInput isInput;
+        final IsId isId;
+
+        if (!request.hasIsInput()) {
+            throw new StatusRuntimeException(Status.INVALID_ARGUMENT.withDescription("422"));
+        }
+
+        isInput = request.getIsInput();
+        if (!isInput.hasIsId()) {
+            throw new StatusRuntimeException(Status.INVALID_ARGUMENT.withDescription("422"));
+        }
+
+        isId = isInput.getIsId();
+        if (!isId.hasIsOutput()) {
+            throw new StatusRuntimeException(Status.INVALID_ARGUMENT.withDescription("422"));
+        }
+
+        final String isStringValue = isId.getIsOutput()
+                .getIsStringValue();
+
+        if (isStringValue.isEmpty()) {
+            throw new StatusRuntimeException(Status.INVALID_ARGUMENT.withDescription("422"));
+        }
+
         responseObserver.onNext(IsIdentifiedCachedItem.newBuilder()
+                .setIsOutput(IsOutput.newBuilder()
+                        .setIsKnownBoolean(general.containsKey(isStringValue)
+                        )
+                        .build())
                 .build());
         responseObserver.onCompleted();
     }
--- src/test/groovy/identifiedcacheditem/identifiedcacheditem/ToIsIdentifiedCachedItemImplBaseImplTest.groovy
+++ src/test/groovy/identifiedcacheditem/identifiedcacheditem/ToIsIdentifiedCachedItemImplBaseImplTest.groovy
@@ -1,22 +1,40 @@
 package identifiedcacheditem.identifiedcacheditem
 
+import com.google.protobuf.ByteString
+import id.id.IsId
+import id.output.IsOutput
+import identifiedcacheditem.input.IsInput
 import io.grpc.StatusRuntimeException
 import io.grpc.inprocess.InProcessChannelBuilder
 import main.Test
+import removedcacheditem.removedcacheditem.NotRemovedCachedItem
+import removedcacheditem.removedcacheditem.ToIsRemovedCachedItemGrpc
 import spock.lang.Shared
 import spock.lang.Specification
+import storedcacheditem.storedcacheditem.NotStoredCachedItem
+import storedcacheditem.storedcacheditem.ToIsStoredCachedItemGrpc
+
+import java.util.concurrent.TimeUnit
 
 class ToIsIdentifiedCachedItemImplBaseImplTest extends Specification {
 
     @Shared
     ToIsIdentifiedCachedItemGrpc.ToIsIdentifiedCachedItemBlockingStub stub
 
+    @Shared
+    ToIsStoredCachedItemGrpc.ToIsStoredCachedItemBlockingStub toIsStoredCachedItemBlockingStub
+
+    @Shared
+    ToIsRemovedCachedItemGrpc.ToIsRemovedCachedItemBlockingStub toIsRemovedCachedItemBlockingStub
+
     def setupSpec() {
         Test.before()
-        stub = ToIsIdentifiedCachedItemGrpc.newBlockingStub(InProcessChannelBuilder.forName(ToIsIdentifiedCachedItemGrpc.SERVICE_NAME).usePlaintext().build()).withWaitForReady()
+        stub = ToIsIdentifiedCachedItemGrpc.newBlockingStub(InProcessChannelBuilder.forName(ToIsIdentifiedCachedItemGrpc.SERVICE_NAME).usePlaintext().build()).withWaitForReady().withDeadlineAfter(1, TimeUnit.MINUTES)
+        toIsStoredCachedItemBlockingStub = ToIsStoredCachedItemGrpc.newBlockingStub(InProcessChannelBuilder.forName(ToIsStoredCachedItemGrpc.SERVICE_NAME).usePlaintext().build()).withWaitForReady().withDeadlineAfter(1, TimeUnit.MINUTES)
+        toIsRemovedCachedItemBlockingStub = ToIsRemovedCachedItemGrpc.newBlockingStub(InProcessChannelBuilder.forName(ToIsRemovedCachedItemGrpc.SERVICE_NAME).usePlaintext().build()).withWaitForReady().withDeadlineAfter(1, TimeUnit.MINUTES)
     }
 
-    def """Should not allow empty"""() {
+    def """Should not allow missing input"""() {
         setup:
         def item = NotIdentifiedCachedItem.newBuilder().build()
 
@@ -25,5 +43,184 @@ class ToIsIdentifiedCachedItemImplBaseImplTest extends Specification {
 
         then:
         thrown StatusRuntimeException
+    }
+
+    def """Should not allow empty input"""() {
+        setup:
+        def item = NotIdentifiedCachedItem.newBuilder()
+                .setIsInput(IsInput.newBuilder().build())
+                .build()
+
+        when:
+        stub.produce(item)
+
+        then:
+        thrown StatusRuntimeException
+    }
+
+    def """Should not allow missing input -> id"""() {
+        setup:
+        def item = NotIdentifiedCachedItem.newBuilder()
+                .setIsInput(IsInput.newBuilder().build())
+                .build()
+
+        when:
+        stub.produce(item)
+
+        then:
+        thrown StatusRuntimeException
+    }
+
+    def """Should not allow empty input -> id"""() {
+        setup:
+        def item = NotIdentifiedCachedItem.newBuilder()
+                .setIsInput(IsInput.newBuilder()
+                        .setIsId(IsId.newBuilder().build())
+                        .build())
+                .build()
+
+        when:
+        stub.produce(item)
+
+        then:
+        thrown StatusRuntimeException
+    }
+
+    def """Should not allow empty input -> id -> output"""() {
+        setup:
+        def item = NotIdentifiedCachedItem.newBuilder()
+                .setIsInput(IsInput.newBuilder()
+                        .setIsId(IsId.newBuilder()
+                                .setIsOutput(IsOutput.newBuilder().build())
+                                .build())
+                        .build())
+                .build()
+
+        when:
+        stub.produce(item)
+
+        then:
+        thrown StatusRuntimeException
+    }
+
+    def """Should not allow empty input -> id -> output -> string value"""() {
+        setup:
+        def item = NotIdentifiedCachedItem.newBuilder()
+                .setIsInput(IsInput.newBuilder()
+                        .setIsId(IsId.newBuilder()
+                                .setIsOutput(IsOutput.newBuilder()
+                                        .setIsStringValue("")
+                                        .build())
+                                .build())
+                        .build())
+                .build()
+
+        when:
+        stub.produce(item)
+
+        then:
+        thrown StatusRuntimeException
+    }
+
+    def """Should succeed on non empty input -> id -> output -> string value"""() {
+        setup:
+        def item = NotIdentifiedCachedItem.newBuilder()
+                .setIsInput(IsInput.newBuilder()
+                        .setIsId(IsId.newBuilder()
+                                .setIsOutput(IsOutput.newBuilder()
+                                        .setIsStringValue(String.valueOf(System.currentTimeMillis()))
+                                        .build())
+                                .build())
+                        .build())
+                .build()
+
+        when:
+        IsIdentifiedCachedItem isIdentifiedCachedItem = stub.produce(item)
+
+        then:
+        assert isIdentifiedCachedItem.hasIsOutput()
+        assert !isIdentifiedCachedItem.getIsOutput().getIsKnownBoolean()
+    }
+
+    def """Should return known on existing key"""() {
+        setup:
+        def key = String.valueOf(System.currentTimeMillis())
+
+        def notStoredCachedItem = NotStoredCachedItem.newBuilder()
+                .setIsInput(storedcacheditem.input.IsInput.newBuilder()
+                        .setIsId(IsId.newBuilder()
+                                .setIsOutput(IsOutput.newBuilder()
+                                        .setIsStringValue(key)
+                                        .build())
+                                .build())
+                        .setIsUseLockBoolean(true)
+                        .setIsItemBytes(ByteString.copyFrom(key, "UTF-8"))
+                        .build())
+                .build()
+
+        def item = NotIdentifiedCachedItem.newBuilder()
+                .setIsInput(IsInput.newBuilder()
+                        .setIsId(IsId.newBuilder()
+                                .setIsOutput(IsOutput.newBuilder()
+                                        .setIsStringValue(key)
+                                        .build())
+                                .build())
+                        .build())
+                .build()
+
+        when:
+        toIsStoredCachedItemBlockingStub.produce(notStoredCachedItem)
+        IsIdentifiedCachedItem isIdentifiedCachedItem = stub.produce(item)
+
+        then:
+        assert isIdentifiedCachedItem.hasIsOutput()
+        assert isIdentifiedCachedItem.getIsOutput().getIsKnownBoolean()
+    }
+
+    def """Should return not known on non existing key"""() {
+        setup:
+        def key = String.valueOf(System.currentTimeMillis())
+
+        def notStoredCachedItem = NotStoredCachedItem.newBuilder()
+                .setIsInput(storedcacheditem.input.IsInput.newBuilder()
+                        .setIsId(IsId.newBuilder()
+                                .setIsOutput(IsOutput.newBuilder()
+                                        .setIsStringValue(key)
+                                        .build())
+                                .build())
+                        .setIsUseLockBoolean(true)
+                        .setIsItemBytes(ByteString.copyFrom(key, "UTF-8"))
+                        .build())
+                .build()
+
+        def notRemovedCachedItem = NotRemovedCachedItem.newBuilder()
+                .setIsInput(removedcacheditem.input.IsInput.newBuilder()
+                        .setIsId(IsId.newBuilder()
+                                .setIsOutput(IsOutput.newBuilder()
+                                        .setIsStringValue(key)
+                                        .build())
+                                .build())
+                        .setIsUseLockBoolean(true)
+                        .build())
+                .build()
+
+        def item = NotIdentifiedCachedItem.newBuilder()
+                .setIsInput(IsInput.newBuilder()
+                        .setIsId(IsId.newBuilder()
+                                .setIsOutput(IsOutput.newBuilder()
+                                        .setIsStringValue(key)
+                                        .build())
+                                .build())
+                        .build())
+                .build()
+
+        when:
+        toIsStoredCachedItemBlockingStub.produce(notStoredCachedItem)
+        toIsRemovedCachedItemBlockingStub.produce(notRemovedCachedItem)
+        IsIdentifiedCachedItem isIdentifiedCachedItem = stub.produce(item)
+
+        then:
+        assert isIdentifiedCachedItem.hasIsOutput()
+        assert !isIdentifiedCachedItem.getIsOutput().getIsKnownBoolean()
     }
 }
